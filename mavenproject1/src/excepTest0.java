6dd5c3e3126f2028257ab0f6b27ef10cb5d14574
peanutxo <93031235+peanutxo@users.noreply.github.com>
2023-02-04 18:13:22 +0530

    Delete mavenproject1 directory

--- mavenproject1/src/excepTest0.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class excepTest0 {
-    public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        System.out.println("Hey buddy! What's your age?");
-        int age= sc.nextInt();
-        System.out.println("Noice! so you're "+age+" old.");
-    }
-}
