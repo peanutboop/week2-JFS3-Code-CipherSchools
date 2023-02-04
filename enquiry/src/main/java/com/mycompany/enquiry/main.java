c5cbb001bfb70b26bb95c512c7610806953612fc
peanutxo <93031235+peanutxo@users.noreply.github.com>
2023-02-04 18:43:57 +0530

    enquiry commit.1

---
+++ enquiry/src/main/java/com/mycompany/enquiry/main.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.enquiry;
+
+/**
+ *
+ * @author ishan
+ */
+public class main {
+    public static void main(String[] args) {
+        
+        NewJFrame app= new NewJFrame();
+        
+        app.setVisible(true);
+    }
+    
+}
